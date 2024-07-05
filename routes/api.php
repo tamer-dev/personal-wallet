a7c5f51ee84b2c2892c137eb375ff68562e682a6
tamer-dev <tamer.dev.alex@gmail.com>
2024-07-05 21:52:40 +0300

    rename routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,17 @@
 
 <?php
 
-use App\Http\Controllers\API\WalletController;
+use App\Http\Controllers\API\WalletTransactionController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['middleware' => ['jwt.auth']], function() {
+//Route::group(['middleware' => ['jwt.auth']], function() {
     Route::prefix('wallet')->group(function () {
-        Route::post('add-funds', [WalletController::class, 'addFunds']);
-        Route::post('transfer', [WalletController::class, 'transferFunds']);
-        Route::get('transactions', [WalletController::class, 'viewTransactions']);
-        Route::post('withdraw', [WalletController::class, 'withdrawFunds']);
-        Route::get('transactions/pdf', [WalletController::class, 'generatePdf']);
-        Route::get('transfer/qr', [WalletController::class, 'generateQrCode']);
+        Route::post('add-funds', [WalletTransactionController::class, 'addFunds']);
+        Route::post('transfer', [WalletTransactionController::class, 'transferFunds']);
+        Route::get('transactions', [WalletTransactionController::class, 'viewTransactions']);
+        Route::post('withdraw', [WalletTransactionController::class, 'withdrawFunds']);
+        Route::get('transactions/pdf', [WalletTransactionController::class, 'generatePdf']);
+        Route::get('transfer/qr', [WalletTransactionController::class, 'generateQrCode']);
     });
-});
+//});
